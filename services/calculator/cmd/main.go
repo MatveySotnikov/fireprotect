69bda58332aad6d6c2a3f41f0466b468635aba54
MatveySotnikov <keklolomg3321@gmail.com>
2026-05-24 12:49:22 +0300

    feat: gRPC health check

--- services/calculator/cmd/main.go
+++ services/calculator/cmd/main.go
@@ -6,8 +6,9 @@ import (
 
 	pb "github.com/MatveySotnikov/fireprotect/gen/calculatorpb"
 	"github.com/MatveySotnikov/fireprotect/services/calculator/internal"
-
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
 )
 
 func main() {
@@ -17,7 +18,13 @@ func main() {
 	}
 
 	s := grpc.NewServer()
+
 	pb.RegisterCalcServiceServer(s, &internal.CalcServiceServer{})
+
+	healthServer := health.NewServer()
+	grpc_health_v1.RegisterHealthServer(s, healthServer)
+	healthServer.SetServingStatus("", grpc_health_v1.HealthCheckResponse_SERVING)
+	log.Println("Health check registered")
 
 	log.Println("Calculator gRPC server listening on :50051")
 	if err := s.Serve(lis); err != nil {
